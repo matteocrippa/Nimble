2ba6913c2fab27367abc3e7ceaf321edff12bf75
Matteo Crippa <matteocrippa@users.noreply.github.com>
2021-12-12 21:21:39 +0100

    update Cwl libs to 2.1.x

--- Carthage/Checkouts/CwlPreconditionTesting/Sources/CwlPreconditionTesting/CwlCatchBadInstruction.swift
+++ Carthage/Checkouts/CwlPreconditionTesting/Sources/CwlPreconditionTesting/CwlCatchBadInstruction.swift
@@ -20,10 +20,11 @@
 
 #if (os(macOS) || os(iOS)) && (arch(x86_64) || arch(arm64))
 
+import CwlCatchException
 import Foundation
 import Swift
 
-#if SWIFT_PACKAGE
+#if SWIFT_PACKAGE || COCOAPODS
 	import CwlMachBadInstructionHandler
 #endif
 
